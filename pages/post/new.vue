872f4eff464a14e8022776665c971852e2b324a6
xiong35 <2242787668@qq.com>
2020-07-31 14:51:08 +0800

    new post

--- pages/post/new.vue
+++ pages/post/new.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card>
     <TopBar title="发布组队"></TopBar>
-    <v-form class="pa-4 pt-7">
+    <v-form lazy-validation class="pa-4 pt-7">
       <v-select
         :items="[
           '科技大赛',
@@ -19,13 +19,15 @@
         v-model="type"
         multiple
         chips
-        label="项目类别"
+        label="项目类别(多选)"
         outlined
+        :rules="[(v) => v.length > 0 || '请选择项目类别']"
       ></v-select>
       <v-text-field
         label="项目名称"
         v-model="title"
         outlined=""
+        :rules="[(v) => !!v || '请填写项目名称']"
       ></v-text-field>
       <v-menu
         ref="menu"
@@ -43,6 +45,7 @@
             v-bind="attrs"
             v-on="on"
             outlined=""
+            :rules="[(v) => !!v || '请填写项目开始时间']"
           ></v-text-field>
         </template>
         <v-date-picker
@@ -67,12 +70,17 @@
         label="项目简介"
         outlined=""
         v-model="description"
+        :rules="[(v) => !!v || '请填写项目简介']"
       ></v-textarea>
 
       <v-text-field
         label="项目人数"
         v-model="number"
         outlined=""
+        :rules="[
+          (v) => !!v || '请填写项目人数',
+          (v) => !!Number(v) || '请输入数字',
+        ]"
       ></v-text-field>
       <v-select
         v-model="period"
@@ -87,26 +95,39 @@
         ]"
         outlined=""
         label="项目周期"
+        :rules="[(v) => !!v || '请选择项目周期']"
       ></v-select>
       <v-textarea
         label="技能要求"
         outlined=""
         v-model="skill"
+        :rules="[(v) => !!v || '请填写技能要求']"
       ></v-textarea>
       <v-select
+        :items="['20级', '19级', '18级', '17级', '16级']"
+        v-model="grade"
+        label="年级要求(多选)"
+        outlined
+        multiple
+        chips
+        :rules="[(v) => v.length > 0 || '请选择年级']"
+      ></v-select>
+      <v-autocomplete
         v-model="major"
         :items="majors"
         outlined=""
-        label="专业要求"
+        label="专业要求(多选)"
         multiple
         chips
+        :rules="[(v) => v.length > 0 || '请选择专业']"
         :menu-props="{ top: true, offsetY: true }"
-      ></v-select>
+      ></v-autocomplete>
       <v-select
         v-model="rank"
         :items="['院级', '校级', '省级', '国家级']"
         label="项目评级"
         outlined=""
+        :rules="[(v) => !!v || '请填写项目评级']"
         :menu-props="{ top: true, offsetY: true }"
       ></v-select>
     </v-form>
@@ -127,6 +148,8 @@
   import TopBar from "~/components/TopBar";
 
   import { majors } from "~/assets/data";
+  import { POST } from "~/network/methods";
+  import { checkSignIn } from "~/utils/validate";
 
   export default {
     transition: "layout",
@@ -147,25 +170,70 @@
     data() {
       return {
         type: [],
-        title: "",
+        title: "联创夏令营",
+        grade: ["19级"],
         dateMenu: false,
         date: "",
-        description: "",
-        number: "",
+        description: "联创团队举办的夏令营, 可以学到很多知识",
+        number: "7",
         period: "",
         rank: "",
-        major: "",
-        skill: "",
+        major: [],
+        skill: "精通c++, java, lisp, brainfuck",
         majors,
       };
     },
     computed: {},
     watch: {},
     methods: {
-      submit() {},
+      async submit() {
+        let {
+          type,
+          title,
+          rank,
+          major,
+          period,
+          date,
+          description,
+          number,
+          grade,
+          skill,
+        } = this;
+
+        let { token, userInfo } = this.$store.state;
+        let { userID } = userInfo;
+
+        grade = grade.join(",");
+
+        let data = {
+          token,
+          publisher: userID,
+          title,
+          type,
+          rank,
+          major,
+          period,
+          beginDate: date.split("-").join("/"),
+          description,
+          memberNum: number * 1,
+          grade,
+          skill,
+          members: [],
+        };
+
+        let res = await POST("/project", data);
+
+        let { id } = res.data;
+
+        alert("提交成功!");
+
+        this.$router.push("/project?id=" + id);
+      },
     },
     created() {},
-    mounted() {},
+    mounted() {
+      checkSignIn(this);
+    },
     async asyncData({ store, query }) {},
   };
 </script>
