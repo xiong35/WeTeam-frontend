7f7455e82772153ddd385323c09c35aba07146a7
xiong35 <2242787668@qq.com>
2020-08-02 20:43:53 +0800

    base color v1.0

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,12 +70,12 @@ export default {
       themes: {
         light: {
           primary: "#32AADE",
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
+          accent: colors.indigo.accent1,
+          secondary: colors.blueGrey.base,
+          info: colors.blue.base,
+          warning: colors.amber.darken1,
+          error: colors.deepOrange.accent3,
+          success: colors.lightGreen.base,
         },
       },
     },
