a78c7e4797f734e73934870ad97af935a14849f6
xiong35 <2242787668@qq.com>
2020-08-04 09:31:38 +0800

    finish 404 page

---
+++ layouts/404.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-img
+    @click="$router.push('/home')"
+    :src="require('~/assets/img/404.jpg')"
+  ></v-img>
+</template>
+
+<script>
+  export default {
+    head() {
+      return { title: "404 not found" };
+    },
+  };
+</script>
+
+<style scoped>
+  h1 {
+    font-size: 20px;
+  }
+</style>
